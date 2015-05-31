1f564dac689d96be8ea66e67f2b849dec6e36998
root <root@mpalade1.mylabserver.com>
2015-05-31 15:44:23 +0000

    Use chef-client.

--- roles/base.rb
+++ roles/base.rb
@@ -1,3 +1,3 @@
 name "base"
 description "contains recipes that should be run on all nodes"
-run_list "recipe[security]", "recipe[localusers]"
+run_list "recipe[chef-client::delete_validation]", "recipe[chef-client]", "recipe[localusers]"
